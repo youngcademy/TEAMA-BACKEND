1c6abe27cb4db5322678b154bdc47e09de25953a
byung ju choi <bjc30849@gmail.com>
2022-03-22 22:00:32 +0900

    fix: 시퀄라이저 주석 처리

--- api/users/user_router.js
+++ api/users/user_router.js
@@ -10,10 +10,10 @@ const {
 const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
 
-// router.post("/", checkToken, createUser);
-// router.get("/", checkToken, getUsers);
-// router.get("/:id", checkToken, getUserByUserID);
-// router.patch("/", checkToken, updateUser);
+router.post("/", checkToken, createUser);
+router.get("/", checkToken, getUsers);
+router.get("/:id", checkToken, getUserByUserID);
+router.patch("/", checkToken, updateUser);
 router.post("/login", login);
 
 module.exports = router;
